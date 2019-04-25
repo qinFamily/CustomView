97600e0d5ae5bfe7d335142ccf8a59d19eed93f5
leavesC <1990724437@qq.com>
2019-04-25 16:57:15 +0800

    clean

--- app/src/main/java/leavesc/hello/customview/MainActivity.java
+++ app/src/main/java/leavesc/hello/customview/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        startActivity(WaveViewActivity.class);
+//        startActivity(WaveViewActivity.class);
     }
 
     private void startActivity(Class clazz) {
