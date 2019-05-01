06798ddb9bd914f7cf1bc7d939fa0bc6af539c44
leavesC <1990724437@qq.com>
2019-05-01 09:36:54 +0800

    配置签名key

--- app/src/main/java/leavesc/hello/customview/MainActivity.java
+++ app/src/main/java/leavesc/hello/customview/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        startActivity(WaveLoadingViewActivity.class);
+//        startActivity(WaveLoadingViewActivity.class);
     }
 
     private void startActivity(Class clazz) {
